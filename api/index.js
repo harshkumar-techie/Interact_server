b32bc1c89be45a38717185217a693ef957a29299
harshkumar-techie <swe.harsh.kumar@gmail.com>
2025-12-24 20:53:45 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -37,6 +37,7 @@ app.post("/login", async (req, res) => {
   try {
     const db = await connectDB();
     await db.collection("user_data").insertOne(req.body);
+    console.log(req.body)
     res.status(200).json({ message: "signup successful" });
   } catch (err) {
     console.error(err);
