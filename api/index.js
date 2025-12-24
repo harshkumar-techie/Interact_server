d8ce9cd59861ad991226ff7fc7be7024c2ea3409
harshkumar-techie <swe.harsh.kumar@gmail.com>
2025-12-24 20:23:31 +0530

    Initial commit

---
+++ api/index.js
@@ -0,0 +1,44 @@
+import express from "express";
+import cors from "cors";
+import { MongoClient, ServerApiVersion } from "mongodb";
+import "dotenv/config";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// MongoDB client (cached for serverless)
+const client = new MongoClient(process.env.MONGO_URL, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+let db;
+
+// Connect once (important for Vercel)
+async function connectDB() {
+  if (db) return db; // prevent reconnecting
+
+  await client.connect();
+  db = client.db("interact");
+  console.log("✅ MongoDB connected");
+  return db;
+}
+
+app.post("/login", async (req, res) => {
+  try {
+    const db = await connectDB();
+    await db.collection("user_data").insertOne(req.body);
+    res.status(200).json({ message: "signup successful" });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "DB insert failed" });
+  }
+});
+
+
+export default app;
