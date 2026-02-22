ddf7441dbfec1279588873586f865258f49cd805
harshkumar-techie <swe.harsh.kumar@gmail.com>
2026-02-22 11:44:14 +0530

    Fixed database errors

--- db.js
+++ db.js
@@ -1,17 +1,22 @@
 import { MongoClient, ServerApiVersion } from "mongodb";
 
-const client = new MongoClient(process.env.MONGO_URL, {
-    serverApi: {
-        version: ServerApiVersion.v1,
-        strict: true,
-        deprecationErrors: true,
-    },
-});
-
+let client;
 let db;
 
 export async function connectDB() {
     if (db) return db; // prevent reconnecting
+
+    if (!process.env.MONGO_URL) {
+        throw new Error("MONGO_URL environment variable is not set");
+    }
+
+    client = new MongoClient(process.env.MONGO_URL, {
+        serverApi: {
+            version: ServerApiVersion.v1,
+            strict: true,
+            deprecationErrors: true,
+        },
+    });
 
     await client.connect();
     db = client.db("interact");
